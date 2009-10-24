ec39fa0a67ea3733a0888830351295221a799fa1
Gavin King <gavin.king@gmail.com>
2009-10-24 00:50:29 +0000

    finish javadoc for @Alternative

--- cdi/src/main/java/javax/enterprise/inject/Alternative.java
+++ cdi/src/main/java/javax/enterprise/inject/Alternative.java
@@ -31,9 +31,40 @@ import java.lang.annotation.Target;
  * applied to a bean class, producer method or field
  * or {@linkplain javax.enterprise.inject.Stereotype stereotype}.</p>
  * 
- * <p>An alternative must be explicitly declared in the 
- * <tt>beans.xml</tt> file if it should be available for 
- * lookup, injection or EL resolution.</p>
+ * <pre>
+ * &#064;Alternative
+ * public class MockOrder extends Order { ... }
+ * </pre>
+ * 
+ * <p>An alternative is never available for injection, 
+ * lookup or EL resolution in a Java EE module or library 
+ * that is not a bean deployment archive (a module or 
+ * library with no <tt>beans.xml</tt> file).</p>
+ * 
+ * <p>Nor is an alternative available for injection, lookup 
+ * or EL resolution in every bean deployment archive. An 
+ * alternative must be explicitly <em>selected</em> in every
+ * bean deployment archive in which the alternative should 
+ * be available for injection, lookup and EL resolution.</p>
+ * 
+ * <p>By default, a bean deployment archive has no selected 
+ * alternatives. An alternative  must be explicitly declared 
+ * using the <tt>&lt;alternatives&gt;</tt> element of the 
+ * <tt>beans.xml</tt> file of the bean deployment archive.</p>
+ * 
+ * <p>The <tt>&lt;alternatives&gt;</tt> element contains a 
+ * list of bean classes and stereotypes. An alternative is 
+ * selected for the bean deployment archive if either:</p>
+ * 
+ * <ul>
+ * <li>the alternative is a managed bean or session bean and 
+ * the bean class of the bean is listed,</li>
+ * <li>the alternative is a producer method, field or resource, 
+ * and the bean class that declares the method or field is listed, 
+ * or</li>
+ * <li>any {@link javax.enterprise.inject.Alternative &#064;Alternative} 
+ * stereotype of the alternative is listed.</li>
+ * </ul>
  * 
  * @author Gavin King
  * @author Pete Muir
