e373f064175237feb7b1f7bde65206bc4de0a908
Gavin King <gavin.king@gmail.com>
2009-10-24 01:50:25 +0000

    add package-info

---
+++ cdi/src/main/java/javax/enterprise/inject/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains annotations relating to bean and 
+ * stereotype definition, built-in qualifiers, and interfaces 
+ * and classes relating to programmatic lookup.
+ * 
+ */
+package javax.enterprise.inject;
