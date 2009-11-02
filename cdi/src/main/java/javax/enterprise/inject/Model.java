673973564565f16d62fc21c62de832507ab6bced
Gavin King <gavin.king@gmail.com>
2009-11-02 16:19:08 +0000

    @Documented

--- cdi/src/main/java/javax/enterprise/inject/Model.java
+++ cdi/src/main/java/javax/enterprise/inject/Model.java
@@ -5,6 +5,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -22,6 +23,7 @@ import javax.inject.Named;
 
 @Named
 @RequestScoped
+@Documented
 @Stereotype
 @Target( { TYPE, METHOD, FIELD })
 @Retention(RUNTIME)
