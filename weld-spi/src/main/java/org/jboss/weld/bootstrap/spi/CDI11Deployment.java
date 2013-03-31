9778aea667cf80b9bb021dfa55114a839c7ed6ad
Jozef Hartinger <jharting@redhat.com>
2013-03-31 10:12:51 +0200

    WELD-1385 Introduce Deployment.getBeanDeploymentArchive(Class<?> beanClass)

---
+++ weld-spi/src/main/java/org/jboss/weld/bootstrap/spi/CDI11Deployment.java
@@ -0,0 +1,32 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.bootstrap.spi;
+
+public interface CDI11Deployment extends Deployment {
+
+    /**
+     * Returns the {@link BeanDeploymentArchive} containing the given class.
+     *
+     * If the deployment archive containing the given class is not currently a bean deployment archive, null is returned. Unlike
+     * {@link #loadBeanDeploymentArchive(Class)}, invocation of this method never results in a new {@link BeanDeploymentArchive}
+     * instance to be created. This method may be called at runtime.
+     *
+     * @param beanClass
+     * @return the {@link BeanDeploymentArchive} containing the bean class or null if no such {@link BeanDeploymentArchive} exists
+     */
+    BeanDeploymentArchive getBeanDeploymentArchive(Class<?> beanClass);
+}
