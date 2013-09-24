6b8d82bdebe39394780eece60645a4e43088191f
Jozef Hartinger <jharting@redhat.com>
2013-09-24 07:33:00 +0200

    Upgrade dependency versions

--- weld-spi/src/test/java/org/jboss/weld/servlet/spi/test/MockHttpServletRequest.java
+++ weld-spi/src/test/java/org/jboss/weld/servlet/spi/test/MockHttpServletRequest.java
@@ -4,15 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 public class MockHttpServletRequest implements HttpServletRequest {
 
@@ -292,6 +301,64 @@ public class MockHttpServletRequest implements HttpServletRequest {
     @Override
     public String getServletPath() {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return null;
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return null;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return false;
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return false;
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest arg0, ServletResponse arg1) throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse arg0) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public Part getPart(String arg0) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public void login(String arg0, String arg1) throws ServletException {
+    }
+
+    @Override
+    public void logout() throws ServletException {
     }
 
 }
