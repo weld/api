a3c26974b378861e029a0b974e735d9761f06eb9
Jozef Hartinger <jharting@redhat.com>
2013-10-08 08:43:13 +0200

    WELD-1523 Expose SPI for fast detection of CDI-compliant classes

---
+++ weld-spi/src/main/java/org/jboss/weld/resources/spi/ClassFileInfo.java
@@ -0,0 +1,118 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.resources.spi;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Inherited;
+
+import javax.enterprise.inject.Vetoed;
+import javax.inject.Inject;
+
+/**
+ * Represents metadata of a Java class. Weld uses this metadata to decide whether the class should be processed by Weld or not without loading the class.
+ *
+ * @author Jozef Hartinger
+ *
+ */
+public interface ClassFileInfo {
+
+    /**
+     * Returns the name of this class.
+     *
+     * @return the FQCN of the class represented by this class file
+     */
+    String getClassName();
+
+    /**
+     * Returns the name of this class's superclass or java.lang.Object if this class does not have a superclass.
+     *
+     * @return the FQCN of the superclass of this class or java.lang.Object if this class does not have a superclass
+     */
+    String getSuperclassName();
+
+    /**
+     * Indicates whether an annotation of the specified annotation type is directly present on this class.
+     *
+     * @param annotationType the specified annotation type
+     * @return true if an annotation of a specified type is directly present on this class, false otherwise
+     */
+    boolean isAnnotationDeclared(Class<? extends Annotation> annotationType);
+
+    /**
+     * Indicates whether this class contains an annotation of the specified annotation type. A class is said to contain the annotation if any of these applies:
+     *
+     * <ul>
+     * <li>The annotation or an annotation annotated with the annotation is present on the class</li>
+     * <li>The annotation or an annotation annotated with the annotation, which is annotated with {@link Inherited}, is present on a direct or indirect
+     * superclass of the given class</li>
+     * <li>The annotation or an annotation annotated with the annotation is present on a field or method declared by the given class or any direct or indirect
+     * superclass of the given class</li>
+     * <li>The annotation or an annotation annotated with the annotation is present on a parameter of a method declared by the given class or any direct or
+     * indirect superclass of the given class</li>
+     * <li>The annotation or an annotation annotated with the annotation is present on a constructor declared by the given class</li>
+     * <li>The annotation or an annotation annotated with the annotation is present on a parameter of a constructor declared by the given class</li>
+     * </ul>
+     *
+     * @param annotationType the specified annotation type
+     * @return true if the class contains the annotation, false otherwise
+     */
+    boolean containsAnnotation(Class<? extends Annotation> annotationType);
+
+    /**
+     * Returns the class access and property modifiers, as defined in http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.1-200-E.1
+     *
+     * @return class access and property modifiers
+     */
+    int getModifiers();
+
+    /**
+     * Indicates whether this class has a CDI constructor.
+     *
+     * @return true if this class has a constructor annotated with {@link Inject} or a no-arg constructor, false otherwise
+     */
+    boolean hasCdiConstructor();
+
+    /**
+     * Indicates whether this class is either the same as, or is a superclass of the specified class.
+     *
+     * @param javaClass the specified class
+     * @return true if this class is either the same as, or is a superclass of the specified class, false otherwise
+     */
+    boolean isAssignableFrom(Class<?> javaClass);
+
+    /**
+     * Indicates whether this class is either the same as, or is a subclass of the specified class.
+     *
+     * @param javaClass the specified class
+     * @return true if this class is either the same as, or is a subclass of the specified class, false otherwise
+     */
+    boolean isAssignableTo(Class<?> javaClass);
+
+    /**
+     * Indicates whether this class is vetoed from CDI processing.
+     *
+     * @return true if the {@link Vetoed} annotation is present on this class or the class's package, false otherwise
+     */
+    boolean isVetoed();
+
+    /**
+     * Indicates whether this class is a top-level class or an inner class.
+     *
+     * @return true if this class is a top-level class, false otherwise
+     */
+    boolean isTopLevelClass();
+}
---
+++ weld-spi/src/main/java/org/jboss/weld/resources/spi/ClassFileServices.java
@@ -0,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.resources.spi;
+
+import org.jboss.weld.bootstrap.api.BootstrapService;
+
+/**
+ * An optional service that provides fast access to Java class metadata without the need to load the given class.
+ *
+ * An integrator may use this service to expose its bytecode scanning facility. If a service implementation is present, Weld uses it to optimize the bootstrap
+ * process. The ClassFileServices implementation is used to determine whether a given Java class fulfills CDI bean requirements instead of loading the class
+ * using a {@link ClassLoader} and examining the result using Java reflection.
+ *
+ * @author Jozef Hartinger
+ *
+ */
+public interface ClassFileServices extends BootstrapService {
+
+    /**
+     * Obtains Java class metadata for a class identified with the specified class name.
+     *
+     * @param className
+     * @return the class metadata
+     * @throws ClassFileInfoException if the service implementation is not able to obtain metadata of a class with the specified name
+     */
+    ClassFileInfo getClassFileInfo(String className);
+}
