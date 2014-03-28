5f01343999a8dcf5b79feb260f4a5c0d1b6c98aa
Jozef Hartinger <jharting@redhat.com>
2014-03-28 10:13:28 +0100

    Add lazy conversation context parameter

--- weld-spi/src/main/java/org/jboss/weld/servlet/api/InitParameters.java
+++ weld-spi/src/main/java/org/jboss/weld/servlet/api/InitParameters.java
@@ -27,4 +27,9 @@ public interface InitParameters {
     String CONTEXT_IGNORE_FORWARD = "org.jboss.weld.context.ignore.forward";
     String CONTEXT_IGNORE_INCLUDE = "org.jboss.weld.context.ignore.include";
     String CONTEXT_MAPPING = "org.jboss.weld.context.mapping";
+
+    /**
+     * Enable / disable lazy initialization of the conversation context.
+     */
+    String CONVERSATION_CONTEXT_LAZY_PARAM = "org.jboss.weld.context.conversation.lazy";
 }
