bd153c19231bc78698b5386a18fb4ab5d7e8c1ca
Matej Novotny <manovotn@redhat.com>
2017-10-16 15:16:45 +0200

    WELD-2433 Backport WeldInvocationContext.

---
+++ weld/src/main/java/org/jboss/weld/interceptor/WeldInvocationContext.java
@@ -0,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.interceptor;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import javax.interceptor.InvocationContext;
+
+/**
+ * Represents an enhanced version of {@link InvocationContext}.
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+public interface WeldInvocationContext extends InvocationContext {
+
+    /**
+     * Return the set of interceptor bindings for a lifecycle callback, business method, timeout method, or constructor.
+     *
+     * <p>
+     * The returned set may be empty if only interceptors using the {@link Interceptors} annotation are associated.
+     * </p>
+     *
+     * @return a set of interceptor bindings
+     */
+    Set<Annotation> getInterceptorBindings();
+
+    /**
+     * Return the set of interceptor bindings with the specified annotation type.
+     *
+     * @param <T>            subclass of {@link Annotation}
+     * @param annotationType type of annotation
+     * @return a set of interceptor bindings with the specified annotation type
+     * @see #getInterceptorBindings()
+     */
+    <T extends Annotation> Set<T> getInterceptorBindingsByType(Class<T> annotationType);
+}
