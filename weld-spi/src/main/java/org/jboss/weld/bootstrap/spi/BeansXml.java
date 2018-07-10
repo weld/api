4647f21cd2a10e8c1a131a7f198af7593f705a46
Matej Novotny <manovotn@redhat.com>
2018-07-10 14:24:30 +0200

    WELD-2515 Add javadoc to BeansXml.getScanning().

--- weld-spi/src/main/java/org/jboss/weld/bootstrap/spi/BeansXml.java
+++ weld-spi/src/main/java/org/jboss/weld/bootstrap/spi/BeansXml.java
@@ -56,6 +56,9 @@ public interface BeansXml {
 
     List<Metadata<String>> getEnabledInterceptors();
 
+    /**
+     * @return Initialized {@link Scanning} instance or {@code Scanning.EMPTY_SCANNING} if empty. Never null.
+     */
     Scanning getScanning();
 
     URL getUrl();
