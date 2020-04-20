af73778043280efba9280482849ca58b61ee922c
Matej Novotny <manovotn@redhat.com>
2020-04-20 15:59:16 +0200

    Fix javadoc still referring to javax Priority.

--- weld/src/main/java/org/jboss/weld/inject/WeldInstance.java
+++ weld/src/main/java/org/jboss/weld/inject/WeldInstance.java
@@ -98,7 +98,7 @@ public interface WeldInstance<T> extends Instance<T> {
     /**
      * The returned comparator sorts handlers by priority in descending order.
      * <ul>
-     * <li>A class-based bean whose annotated type has {@code javax.annotation.Priority} has the priority of value {@code javax.annotation.Priority#value()}</li>
+     * <li>A class-based bean whose annotated type has {@code jakarta.annotation.Priority} has the priority of value {@code jakarta.annotation.Priority#value()}</li>
      * <li>A custom bean which implements {@link Prioritized} has the priority of value {@link Prioritized#getPriority()}</li>
      * <li>Any other bean has the priority of value 0</li>
      * </ul>
