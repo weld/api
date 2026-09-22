a95303c981d2503655ed33ebbe20022fcbf44f9b
Matej Novotny <manovotn@ibm.com>
2026-09-22 14:29:32 +0200

    Remove deprecated WeldInstance handler API
    Remove the handler methods and nested Handler type deprecated since 2021.
    Use standard CDI handles and the existing handle priority comparator.
    Retain Weld-specific runtime Type selection and covariant select methods.

--- weld/src/main/java/org/jboss/weld/inject/WeldInstance.java
+++ weld/src/main/java/org/jboss/weld/inject/WeldInstance.java
@@ -19,14 +19,9 @@ package org.jboss.weld.inject;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.Comparator;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import jakarta.enterprise.context.Dependent;
-import jakarta.enterprise.inject.AmbiguousResolutionException;
 import jakarta.enterprise.inject.Instance;
-import jakarta.enterprise.inject.UnsatisfiedResolutionException;
-import jakarta.enterprise.inject.spi.Bean;
 import jakarta.enterprise.inject.spi.Prioritized;
 import jakarta.enterprise.util.TypeLiteral;
 
@@ -34,11 +29,9 @@ import jakarta.enterprise.util.TypeLiteral;
  * Represents an enhanced version of {@link Instance}.
  *
  * <p>
- * In the following example we filter out beans which are not {@link Dependent} then sort the beans by priority and use the
- * handler whose bean has the highest
- * priority (according to {@link #getPriorityComparator()}) to obtain the hello string. Note that contextual references for
- * beans with lower priority are not
- * created at all.
+ * In the following example we filter out beans which are not {@link Dependent}, sort the beans by priority, and use the
+ * handle whose bean has the highest priority (according to {@link #getHandlePriorityComparator()}) to obtain the hello string.
+ * Contextual references for beans with lower priority are not created at all.
  * </p>
  *
  * <pre>
@@ -49,8 +42,8 @@ import jakarta.enterprise.util.TypeLiteral;
  *     WeldInstance&lt;HelloProvider&gt; instance;
  *
  *     String hello() {
- *         HelloProvider helloProvider = instance.handlersStream().filter(h -&gt; h.getBean().getScope().equals(Dependent.class))
- *                 .sorted(instance.getPriorityComparator()).findFirst().map(Handler::get).orElse(null);
+ *         HelloProvider helloProvider = instance.handlesStream().filter(h -&gt; h.getBean().getScope().equals(Dependent.class))
+ *                 .sorted(instance.getHandlePriorityComparator()).findFirst().map(Instance.Handle::get).orElse(null);
  *         if (helloProvider != null)
  *             return helloProvider.getHello();
  *         return "No hello provider found!";
@@ -63,70 +56,6 @@ import jakarta.enterprise.util.TypeLiteral;
  * @param <T> the required bean type
  */
 public interface WeldInstance<T> extends Instance<T> {
-
-    /**
-     * This method is deprecated as a similar functioning method exists in CDI 4.0 and newer.
-     * Users should instead use {@link Instance#getHandle()}.
-     *
-     * Obtains an initialized contextual reference handler for the bean that has the required type and required qualifiers and
-     * is eligible for injection.
-     *
-     * <p>
-     * The contextual reference is obtained lazily, i.e. when first needed.
-     * </p>
-     *
-     * @return a new handler
-     * @throws UnsatisfiedResolutionException if there is no bean with given type and qualifiers
-     * @throws AmbiguousResolutionException if there is more than one bean given type and qualifiers
-     */
-    @Deprecated
-    Handler<T> getHandler();
-
-    /**
-     * This method is deprecated as a similar functioning method exists in CDI 4.0 and newer.
-     * Users should instead use {@link Instance#handles()}.
-     *
-     * Allows to iterate over contextual reference handlers for all the beans that have the required type and required
-     * qualifiers and are eligible
-     * for injection.
-     *
-     * <p>
-     * Note that the returned {@link Iterable} is stateless and so each {@link Iterable#iterator()} produces a new set of
-     * handlers.
-     * </p>
-     *
-     * @return a new iterable
-     */
-    @Deprecated
-    Iterable<Handler<T>> handlers();
-
-    /**
-     * This method is deprecated as a similar functioning method exists in CDI 4.0 and newer.
-     * Users should instead use {@link Instance#handlesStream()}.
-     *
-     * @return a new stream of contextual reference handlers
-     */
-    @Deprecated
-    default Stream<? extends Handler<T>> handlersStream() {
-        return StreamSupport.stream(handlers().spliterator(), false);
-    }
-
-    /**
-     * This method is deprecated in favor of {@link WeldInstance#getHandlePriorityComparator()} which operates on
-     * a non-deprecated {@link Instance.Handle} interface.
-     *
-     * The returned comparator sorts handlers by priority in descending order.
-     * <ul>
-     * <li>A class-based bean whose annotated type has {@code jakarta.annotation.Priority} has the priority of value
-     * {@code jakarta.annotation.Priority#value()}</li>
-     * <li>A custom bean which implements {@link Prioritized} has the priority of value {@link Prioritized#getPriority()}</li>
-     * <li>Any other bean has the priority of value 0</li>
-     * </ul>
-     *
-     * @return a comparator instance
-     */
-    @Deprecated
-    Comparator<Handler<?>> getPriorityComparator();
 
     /**
      * The returned comparator sorts handles by priority in descending order.
@@ -167,58 +96,5 @@ public interface WeldInstance<T> extends Instance<T> {
      *         {@link java.lang.Object}
      */
     <X> WeldInstance<X> select(Type subtype, Annotation... qualifiers);
-
-    /**
-     * This interface is deprecated.
-     * CDI 4.0 introduced {@link Instance.Handle} interface that offers the same functionality and can be used in place
-     * of Weld specific {@link WeldInstance.Handler}.
-     *
-     * This interface represents a contextual reference handler.
-     * <p>
-     * Allows to inspect the metadata of the relevant bean and also to destroy the underlying contextual instance.
-     * </p>
-     *
-     * @author Martin Kouba
-     * @param <T> the required bean type
-     */
-    @Deprecated
-    interface Handler<T> extends Handle<T> {
-
-        /**
-         * The contextual reference is obtained lazily, i.e. when first needed.
-         *
-         * @return the contextual reference
-         * @see Instance#get()
-         * @throws IllegalStateException If the producing {@link WeldInstance} does not exist
-         */
-        T get();
-
-        /**
-         *
-         * @return the bean metadata
-         */
-        Bean<T> getBean();
-
-        /**
-         * Destroy the contextual instance.
-         *
-         * It's a no-op if:
-         * <ul>
-         * <li>called multiple times</li>
-         * <li>if the producing {@link WeldInstance} does not exist</li>
-         * <li>if the handler does not hold a contextual reference, i.e. {@link #get()} was never called</li>
-         * </ul>
-         *
-         * @see Instance#destroy(Object)
-         */
-        void destroy();
-
-        /**
-         * Delegates to {@link #destroy()}.
-         */
-        @Override
-        void close();
-
-    }
 
 }
